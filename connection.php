36358b098f34ac8efdbc67fcc73bbe2839c39978
Darby2021 <cn1916716@gmail.com>
2022-10-19 09:53:10 +0700

    Data base connect Shop

--- connection.php
+++ connection.php
@@ -1,4 +1,6 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root','','shoptoy')
-            or die("Can not conncect database".mysqli_connect_error());
+$Connect = pg_connect("postgres://ivwufnuxemonov:c940d25d60efad6fdf4497d72d4f9ea3a3835f6c02d270282b0ed8ba526e2e0a@ec2-52-207-15-147.compute-1.amazonaws.com:5432/d576ln3hnra3q8");	
+if (!$Connect) {
+    die("Connection failed");
+}
 ?>
